ea02dc7eba04b0d7bb6c22ff70fc2aa803d4f038
Squirrel <thehillsidetownship@gmail.com>
2022-08-19 20:32:57 +0000

    📥 Archive Cards (Works!)

--- app.js
+++ app.js
@@ -251,7 +251,7 @@ app.post("/newproject", (req, res) => {
 });
 
 /* =============
-// New Card
+// Cards
 ============= */
 
 app.post("/newcard", (req, res) => {
@@ -279,6 +279,30 @@ app.post("/newcard", (req, res) => {
             else { console.log("Result:", yay); }
          }
    );
+});
+
+app.post("/archive-card", (req, res) => {
+   ProjectData.findOne({ id: req.body.projectId }, function (err, docs) {
+      if (err) { console.log(err); }
+      else {
+         let thedata = docs.data.find(x => x.id == req.body.cardId);
+
+         ProjectData.findOneAndUpdate( { id: req.body.projectId, "data.id": thedata.id },
+         { $set: { "data.$.status": "archived",  } }, function(err, yay) {
+            if (err) { console.log(err);}
+            else { console.log(yay, "Successfully archived card!"); }
+         });
+      }
+   });
+
+
+   // Chat.deleteOne( { _id: req.body.msgId }, (err) => { if (err) return console.error(err); } );
+   // Chat.findOne({ name: signedInUser.name }, (err, doc) => {
+   //    if (err) return console.error(err);
+   //    else { res.send("Deleted Successfully."); }
+   // });
+
+   // var result = jsObjects.filter(obj => { return obj.b === 6; });
 });
 
 /* =============
